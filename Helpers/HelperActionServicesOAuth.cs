5c575194093d3bd987a6d2b333b5635e5864eb8b
marsett <jimenezmarset@gmail.com>
2025-05-19 20:43:29 +0200

    Add project files.

---
+++ Helpers/HelperActionServicesOAuth.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ZuvoPetApiAWS.Helpers
+{
+    public class HelperActionServicesOAuth
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string SecretKey { get; set; }
+        public HelperActionServicesOAuth(string issuer, string audience, string secretKey)
+        {
+            this.Issuer = issuer;
+            this.Audience = audience;
+            this.SecretKey = secretKey;
+        }
+        // Necesitamos un método para generar el token
+        // dicho token se basa en nuestro secret key
+        public SymmetricSecurityKey GetKeyToken()
+        {
+            // Convertimos el scret key a bytes
+            byte[] data = Encoding.UTF8.GetBytes(this.SecretKey);
+            // Devolvemos la key generada a partir de los bytes
+            return new SymmetricSecurityKey(data);
+        }
+        // Esta clase la hemos creado también para quitar código
+        // del program
+        public Action<JwtBearerOptions> GetJwtBearerOptions()
+        {
+            Action<JwtBearerOptions> options =
+                new Action<JwtBearerOptions>(options =>
+                {
+                    // Indicamos que debemos validar para el token
+                    options.TokenValidationParameters =
+                    new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = this.Issuer,
+                        ValidAudience = this.Audience,
+                        IssuerSigningKey = this.GetKeyToken()
+                    };
+                });
+            return options;
+        }
+        // Toda seguridad siempre está basada en un schema
+        public Action<AuthenticationOptions> GetAuthenticateSchema()
+        {
+            Action<AuthenticationOptions> options =
+                new Action<AuthenticationOptions>(options =>
+                {
+                    options.DefaultScheme =
+                    JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme =
+                    JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultAuthenticateScheme =
+                    JwtBearerDefaults.AuthenticationScheme;
+                });
+            return options;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+using Microsoft.EntityFrameworkCore;
+using ZuvoPetApiAWS.Repositories;
+using ZuvoPetApiAWS.Data;
+using System.Text.Json.Serialization;
+using Scalar.AspNetCore;
+using ZuvoPetApiAWS.Helpers;
+using Azure.Storage.Blobs;
+using ZuvoPetApiAWS.Services;
+using Microsoft.Extensions.Azure;
+using Azure.Security.KeyVault.Secrets;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAzureClients(factory =>
+{
+    factory.AddSecretClient
+    (builder.Configuration.GetSection("KeyVault"));
+});
+
+SecretClient secretClient = builder.Services.BuildServiceProvider().GetRequiredService<SecretClient>();
+
+KeyVaultSecret secretConnectionString = await secretClient.GetSecretAsync("SqlZuvoPet");
+KeyVaultSecret secretStorageAccount = await secretClient.GetSecretAsync("StorageAccount");
+
+KeyVaultSecret secretAudience = await secretClient.GetSecretAsync("Audience");
+KeyVaultSecret secretIssuer = await secretClient.GetSecretAsync("Issuer");
+KeyVaultSecret secretIterate = await secretClient.GetSecretAsync("Iterate");
+KeyVaultSecret secretKey = await secretClient.GetSecretAsync("Key");
+KeyVaultSecret secretSalt = await secretClient.GetSecretAsync("Salt");
+KeyVaultSecret secretSecretKey = await secretClient.GetSecretAsync("SecretKey");
+
+HelperCriptography.Initialize(
+    secretSalt.Value,
+    secretIterate.Value,
+    secretKey.Value
+);
+builder.Services.AddHttpContextAccessor();
+
+HelperActionServicesOAuth helper = new HelperActionServicesOAuth(
+    secretIssuer.Value,
+    secretAudience.Value,
+    secretSecretKey.Value
+);
+builder.Services.AddSingleton<HelperActionServicesOAuth>(helper);
+builder.Services.AddSingleton<ServiceStorageBlobs>();
+builder.Services.AddScoped<HelperUsuarioToken>();
+builder.Services.AddAuthentication(helper.GetAuthenticateSchema())
+    .AddJwtBearer(helper.GetJwtBearerOptions());
+// Add services to the container.
+
+string azureKeys = secretStorageAccount.Value;
+BlobServiceClient blobServiceClient = new BlobServiceClient(azureKeys);
+builder.Services.AddTransient<BlobServiceClient>(x => blobServiceClient);
+
+string connectionString =
+    secretConnectionString.Value;
+builder.Services.AddTransient<IRepositoryZuvoPet, RepositoryZuvoPet>();
+builder.Services.AddDbContext<ZuvoPetContext>
+    (options => options.UseSqlServer(connectionString));
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.WriteIndented = true;
+    });
+
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    
+}
+app.MapOpenApi();
+app.UseHttpsRedirection();
+
+app.MapScalarApiReference();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", context => {
+    context.Response.Redirect("/scalar/v1");
+    return Task.CompletedTask;
+});
+
+app.Run();
